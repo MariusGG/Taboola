4eb025ff4d324841769bfacf582d34854c7737b9
Marius Guei <marius.g.guei@gmail.com>
2019-04-19 06:39:15 +0100

    update to html and js code

--- src/taboolaWidget.js
+++ src/taboolaWidget.js
@@ -1,22 +1,31 @@
-//ADDING H1 FOR A TITLE
-let getTaboolaWidgets = document.querySelector('#taboola-widgets').children;
+// //ADDING H1 FOR A IMAGE, TITLE & BRAND TAGS
+// const getTaboolaWidgets = document.querySelector('#taboola-widgets').children;
+//
+// for(i = 0; i < getTaboolaWidgets.length; i++) {
+//   let x = getTaboolaWidgets[i].children;
+//   for(e = 0; e < x.length; e++){
+//       let createTitle = document.createElement('h1');
+//       let createBranding = document.createElement('p');
+//       let createImg = document.createElement('img');
+//       createTitle.setAttribute('class', 'title');
+//       createBranding.setAttribute('class', 'branding');
+//       createImg.setAttribute('class', 'images');
+//
+//
+//       (x[e].append(createImg, createTitle, createBranding ));
+//     }
+//   }
+//
+// const getTaboolaWidgets = document.querySelector('#taboola-widgets').children;
+//
+// const getImagetags = document.querySelectorAll('img');
+// getImagetags.forEach((item) => {
+//   console.log(item)
+// })
 
-for(i = 0; i < getTaboolaWidgets.length; i++) {
-  let x = getTaboolaWidgets[i].children;
-  for(e = 0; e < x.length; e++){
-      let createTitle = document.createElement('h1');
-      let createBranding = document.createElement('p');
-      createTitle.setAttribute('class', 'title');
-      createBranding.setAttribute('class', 'brand');
-      (x[e].append(createTitle, createBranding ));
-    }
-  }
-  //
-  // for(i = 0; i < getTaboolaWidgets.length; i++) {
-  //   let x = getTaboolaWidgets[i].children;
-  //   for(e = 0; e < x.length; e++){
-  //       let createTitle = document.createElement('h');
-  //       createTitle.setAttribute("class", "title");
-  //       (x[e].appendChild(createTitle));
-  //     }
-  //   }
+// let x = Array.from(getImagetags)
+//  for(a = 0; a < x.length; a++) {
+//    let createAnchor = document.createElement('a');
+//    createAnchor.appendChild(x[a])
+//    console.log(x[a]);
+//  }
