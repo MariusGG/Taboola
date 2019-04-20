c4445542b3fed64610b985ee7255f8ecdb599bde
Marius Guei <marius.g.guei@gmail.com>
2019-04-20 16:52:56 +0100

    updating script and html

--- src/taboolaWidget.js
+++ src/taboolaWidget.js
@@ -1,6 +1,7 @@
 const api = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init';
 
-  fetch(api)
+function getData(url) {
+  fetch(url)
      .then(function (response) {
           return response.json();
       })
@@ -10,3 +11,21 @@ const api = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get
       .catch(function (err) {
           console.log("Something went wrong!", err);
       });
+}
+getData(api);
+
+let widgetDisplayBoard = document.querySelector('#taboola-widgets');
+
+function createTextNode(text) {
+  return document.createTextNode(text);
+}
+function createHtmlTag(tag) {
+  return document.createElement(tag);
+}
+function appendingHtmlTag(parentTag, childTag) {
+  return parentTag.appendChild(childTag);
+}
+
+function displayData() {
+  
+}
